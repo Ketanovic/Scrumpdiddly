78069327ece3c6438c442d11754255a219d33623
Kirsten <kirstenkwon@gmail.com>
2023-08-24 11:04:05 -0600

    on pantryitem

--- ghi/src/PantryItem.js
+++ ghi/src/PantryItem.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
 
+
 function PantryForm() {
     const [name, setName] = useState('');
-    const [ingredients, setIngredients] = useState('')
+    const [ingredients, setIngredients] = useState([])
+    const [ingredientss, setIngredientss] = useState('')
     const [filter, setFilter] = useState('')
     const [searchIngredient, setSearchIngredient] = useState('')
     const handleChange = (event) => {
         const value = event.target.value;
-        setName(value);
+        setIngredientss(value);
     };
 
     async function fetchIngredients(){
-        const response = await fetch("http://localhost:8000/api/ingredients");
+        const response = await fetch("http://localhost:8000/api/ingredients/");
         if (response.ok) {
             const data = await response.json();
             setIngredients(Object.keys(data.ingredients));
@@ -26,10 +28,10 @@ function PantryForm() {
         setFilter(result)
     }
 
-
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const data = {"name": name}
+        const data = {"name":name}
+        //data.ingredients = ingredientss
         const url = 'http://localhost:8000/api/pantry_item/';
         const fetchConfig = {
             method: "post",
@@ -41,6 +43,7 @@ function PantryForm() {
         const response = await fetch(url, fetchConfig);
         if (response.ok) {
             setName('');
+            //setIngredients('');
         } else {
             console.error(response);
         }
@@ -55,27 +58,26 @@ function PantryForm() {
             <div className="offset-3 col-6">
                 <div className="shadow p-4 mt-4">
                     <h1>Create a Pantry Item</h1>
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit} id="create-ingredient-form">
                         <div className="mb-3">
                             <label htmlFor="name">Pantry Name</label>
-                            <input required onChange={handleChange} value={name} name="name" id="name" type="text" className="form-control" />
+                            <input required onChange={handleSubmit} value={name} name="name" id="name" type="text" className="form-control" />
                         </div>
                         <h1>What Ingredients Do You Have?</h1>
                         <div className="mb-3">
                             <label htmlFor="name">Ingredients</label>
-                            <select onChange={handleSearch} value={ingredients} required name="ingredients" id="ingredients" className="form-control" />
+                            <select onChange={handleChange} value={ingredientss} name="ingredients" id="ingredients" className="form-control" />
                             <option value="">Choose an ingredient</option>
                             {ingredients.map(ingredient => {
-                                return (
-                                    <option key={ingredient.name} value={ingredient.name}>
-                                        {ingredient.name}
+                                  return (
+                                    <option value={ingredient.name} key={ingredient.name}>
+                                    {ingredient.name}
                                     </option>
-                                )
+                                );
                             })}
                         </div>
                         <button className="btn btn-primary">Create</button>
                     <h1>Input Ingredients you have at Home!</h1>
-
                     </form>
                 </div>
             </div>
