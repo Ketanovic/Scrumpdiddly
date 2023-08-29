777649bd8688d3fbe9e3eee5fd7d8f880fceafaa
Derrik <derrikharp@gmail.com>
2023-08-29 09:18:31 -0700

    protected routes to be logged in

--- api/routers/pantry_item.py
+++ api/routers/pantry_item.py
@@ -1,7 +1,5 @@
-from fastapi import (
-    APIRouter,
-    Depends
-)
+from fastapi import APIRouter, Depends
+from authenticator import authenticator
 from models import PantryItemIn, PantryItemOut, PantryItems
 from queries.pantry_item import PantryItemQueries
 
@@ -11,16 +9,16 @@ router = APIRouter()
 
 @router.get("/api/pantry_item", response_model=PantryItems)
 def list_pantry_items(
-    queries: PantryItemQueries = Depends()
+    queries: PantryItemQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data)
 ):
-    return {
-        "pantry_items": queries.find_all()
-    }
+    return {"pantry_items": queries.find_all()}
 
 
 @router.post("/api/pantry_item", response_model=PantryItemOut)
 def create_pantry_item(
     pantry_item_in: PantryItemIn,
-    queries: PantryItemQueries = Depends()
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    queries: PantryItemQueries = Depends(),
 ):
     return queries.create(pantry_item_in=pantry_item_in)
