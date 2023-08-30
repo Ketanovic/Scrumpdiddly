92ba6ffdfef4d4a1d93538d86c5b507bca8bf9a2
Emily Korthals <ejkorthals@gmail.com>
2023-08-30 16:42:02 -0500

    Fixed up some CSS

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -2,12 +2,14 @@ import scrumdiddly from './scrumdiddly.png'
 
 function MainPage() {
   return (
-    <div className='center page-wrap'>
-        <img src={scrumdiddly} className='logo' />
-        <p className="lead mb-4">
-          The premiere solution for making dinner at home easier!
-        </p>
-    </div>
+      <div className='center page-wrap'>
+        <div className='bg-img'>
+          <img src={scrumdiddly} className='logo' />
+          <p className="lead mb-4">
+            The premiere solution for making dinner at home easier!
+          </p>
+        </div>
+      </div>
   );
 }
 
