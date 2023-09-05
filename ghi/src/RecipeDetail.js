c074a5748d27da8e2dc7af863095ebd026a6d6f5
Armin <ketanovic@icloud.com>
2023-09-05 14:49:23 -0500

    Recipe Detail Page edited.

--- ghi/src/RecipeDetail.js
+++ ghi/src/RecipeDetail.js
@@ -3,32 +3,40 @@ import { useParams } from 'react-router-dom';
 
 function RecipeDetailPage() {
   const { recipeName } = useParams();
-  const [recipe, setRecipe] = useState(null);
-  const [error, setError] = useState(false);
+  const [recipe, setRecipe] = useState([]);
+
+  const fetchRecipe = async () => {
+    const url = "http://localhost:8000/api/recipes/";
+    const response = await fetch(url);
+    if (response.ok) {
+      const json = await response.json();
+      setRecipe(json.recipe);
+    }
+  };
 
   useEffect(() => {
-    fetch(`http://localhost:8000/api/recipes/${encodeURIComponent(recipeName)}`)
-      .then(response => response.json())
-      .then(data => {
-        if (data.recipes && data.recipes.length > 0) {
-          setRecipe(data.recipes[0]);
-        } else {
-          setError(true);
-        }
-      })
-      .catch(error => {
-        console.error('Error fetching recipe:', error);
-        setError(true);
-      });
-  }, [recipeName]);
+    fetchRecipe();
+  }, []);
 
-  if (error) {
-    return <div>Recipe not found or an error occurred.</div>;
-  }
+  // useEffect(() => {
+  //   fetch(`http://localhost:8000/api/recipes/${encodeURIComponent(recipeName)}`)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       if (data.recipes && data.recipes.length > 0) {
+  //         setRecipe(data.recipes[0]);
+  //       } else {
+  //         setError(true);
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.error('Error fetching recipe:', error);
+  //       setError(true);
+  //     });
+  // }, [recipeName]);
 
-  if (!recipe) {
-    return <div>Loading...</div>;
-  }
+  // if (!recipe) {
+  //   return <div>Loading...</div>;
+  // }
 
   const ingredients = [];
   for (let i = 1; i <= 20; i++) {
